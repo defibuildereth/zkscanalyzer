c6d2df708a8fce6b044090118d755ce7b4043f44
d7o1d1s0 <d7sarmy@gmail.com>
2022-02-03 14:58:56 -0500

    parsing token balance info to frontend

--- src/containers/ZkscanContainer.js
+++ src/containers/ZkscanContainer.js
@@ -3,18 +3,44 @@ import AddressForm from '../components/AddressForm';
 
 const ZkscanContainer = () => {
 
-    const [address, setAddress] = useState("")
+
+    const [address, setAddress] = useState("");
+    const [info, setInfo] = useState("");
+
+    useEffect(() => {
+        searchAddress(address)
+    }, [address])
+
+    useEffect(() => {
+        console.log(info)
+        // console.log(Object.entries(info))
+    }, [info])
+
 
     const onAddressFormSubmit = (submittedAddress) => {
         console.log('Address Submitted: ', submittedAddress)
         setAddress(submittedAddress.submittedAddress)
     }
 
+    const searchAddress = (address) => {
+        fetch(`https://api.zksync.io/api/v0.2/accounts/${address}`)
+            .then((res) => res.json())
+            .then(data => setInfo(Object.entries(data.result.finalized.balances)))
+    }
+
+
+    const nodes = info.map(item => {
+        return <p>Token: {item[0]} Balance: {item[1]}</p>
+    })
+
     return (
         <>
             <h3>zkscan container</h3>
-            <AddressForm onAddressFormSubmit={onAddressFormSubmit}/>
-            <p>{address ? address: null}</p>
+            <AddressForm onAddressFormSubmit={onAddressFormSubmit} />
+            <h2>{address ? address : null}</h2>
+            <p>{info ? <section>
+                {nodes}
+            </section> : null}</p>
         </>
     )
 
