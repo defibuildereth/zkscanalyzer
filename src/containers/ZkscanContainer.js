5c287e496f289dabfc979fa467dad39c7ab439f9
d7o1d1s0 <d7sarmy@gmail.com>
2022-02-03 23:50:03 -0500

    extracting decimals

--- src/containers/ZkscanContainer.js
+++ src/containers/ZkscanContainer.js
@@ -3,9 +3,9 @@ import AddressForm from '../components/AddressForm';
 
 const ZkscanContainer = () => {
 
-
     const [address, setAddress] = useState("");
     const [info, setInfo] = useState("");
+    const [decimals, setDecimals] = useState([]);
 
     useEffect(() => {
         searchAddress(address)
@@ -13,9 +13,14 @@ const ZkscanContainer = () => {
 
     useEffect(() => {
         console.log(info)
+        parsePrices(info)
+        // parsePrices(info)
         // console.log(Object.entries(info))
     }, [info])
 
+    useEffect(() => {
+        console.log(decimals)
+    }, [decimals])
 
     const onAddressFormSubmit = (submittedAddress) => {
         console.log('Address Submitted: ', submittedAddress)
@@ -35,17 +40,49 @@ const ZkscanContainer = () => {
         return nodes
     }
 
+    const parsePrices = async function (info) {
+        let decimalsPromiseArray = []
 
-return (
-    <>
-        <h3>zkscan container</h3>
-        <AddressForm onAddressFormSubmit={onAddressFormSubmit} />
-        <h2>{address ? address : null}</h2>
-        <p>{info ? <section>
-            {parseInfo(info)}
-        </section> : null}</p>
-    </>
-)
+        for (let i = 0; i < info.length; i++) {
+            let token = info[i][0];
+            decimalsPromiseArray.push(getTokenDecimal(token))
+        }
+
+        console.log('decimals promise array: ', decimalsPromiseArray)
+
+        await Promise.all(decimalsPromiseArray)
+            .then((values) => {
+                console.log('promise array result: ', values)
+                // .then(setDecimals(decimalsArray))
+            }
+            )
+    }
+
+    const getTokenDecimal = async function (token) {
+        let decimal;
+        // console.log('query: ', `https://api.zksync.io/api/v0.2/tokens/${token}/priceIn/usd`)
+        await fetch(`https://api.zksync.io/api/v0.2/tokens/${token}/priceIn/usd`)
+            .then((res) => res.json())
+            .then(data => {
+                console.log('get decimal result: ', token, data.result.decimals)
+                decimal = data.result.decimals
+            })
+        
+        return ({token: token, decimal: decimal})
+    }
+
+
+    return (
+        <>
+            <h3>zkscan container</h3>
+            <AddressForm onAddressFormSubmit={onAddressFormSubmit} />
+            <h2>{address ? address : null}</h2>
+            <p>{info ? <section>
+                {parseInfo(info)}
+            </section> : null}</p>
+            <p>{decimals ? decimals : null}</p>
+        </>
+    )
 
 }
 
