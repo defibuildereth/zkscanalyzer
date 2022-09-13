ca5e7b8c23c6469e5e9ed492d3c0c76a35481d70
d7o1d1s0 <d7sarmy@gmail.com>
2022-09-13 17:02:33 +0100

    footer links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Footer';
 import ZkscanContainer from "./containers/ZkscanContainer";
 
 function App() {
   return (
     <div id="my-component">
       <ZkscanContainer/>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = ({ }) => {
+
+
+    return (
+        <>
+        <div id='footer' className='relative bottom-0 flex flex-col pt-6'>
+            <a className='text-xs' href='https://defibuilder.eth.limo/'>DefiBuilder.eth</a>
+            <a className='text-xs' href='https://www.zigzagstats.com/'>ZigZagStats.com</a>
+            <a className='text-xs' href='https://www.defiyieldcalculator.com/'>DefiYieldCalculator.com</a>
+        </div>
+        </>
+    )
+}
+
+export default Footer
